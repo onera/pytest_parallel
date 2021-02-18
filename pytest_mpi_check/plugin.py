d321e8899059ec1e20f7af13649b53239c974e19
Bruno Maugars <bruno.maugars@onera.fr>
2021-02-18 16:16:12 +0100

    Update with pytest >= 6.2.0

--- pytest_mpi_check/plugin.py
+++ pytest_mpi_check/plugin.py
@@ -9,12 +9,12 @@ from .html_mpi     import HTMLReportMPI
 from .junit_mpi    import LogXMLMPI
 from .terminal_mpi import TerminalReporterMPI
 from .mpi_reporter import MPIReporter
+from _pytest.junitxml import xml_key
 
 from .utils import get_n_proc_for_test, prepare_subcomm_for_tests
 
 from _pytest          import deprecated
 from _pytest          import runner
-from _pytest.warnings import _issue_warning_captured
 
 from _pytest._code.code import ExceptionChainRepr
 from _pytest.terminal   import TerminalReporter
@@ -44,8 +44,6 @@ def pytest_collection_modifyitems(config, items):
   # > This fonction do a kind of sheduling because if comm is null the test is not executed
   prepare_subcomm_for_tests(items)
 
-  # with_mpi = config.getoption(WITH_MPI_ARG)
-  # only_mpi = config.getoption(ONLY_MPI_ARG)
   # Rajouter nombre proc max dans les infos
   for item in items:
 
@@ -142,20 +140,16 @@ def pytest_configure(config):
 
   # --------------------------------------------------------------------------------
   # Prevent previous load of other pytest_html
-  xml = getattr(config, "_xml", None)
+  xml = config._store.get(xml_key, None)
   if xml:
-    del config._xml
-    config.pluginmanager.unregister(xml)
+      del config._store[xml_key]
+      config.pluginmanager.unregister(xml)
 
   xmlpath = config.option.xmlpath
-  # prevent opening xmllog on slave nodes (xdist)
-  if xmlpath and not hasattr(config, "slaveinput"):
+  # Prevent opening xmllog on worker nodes (xdist).
+  if xmlpath and not hasattr(config, "workerinput"):
       junit_family = config.getini("junit_family")
-      junit_family = "xunit2"
-      if not junit_family:
-          _issue_warning_captured(deprecated.JUNIT_XML_DEFAULT_FAMILY, config.hook, 2)
-      _issue_warning_captured(deprecated.JUNIT_XML_DEFAULT_FAMILY, config.hook, 2)
-      config._xml = LogXMLMPI(
+      config._store[xml_key] = LogXMLMPI(
           comm,
           config._mpi_reporter,
           xmlpath,
@@ -166,7 +160,7 @@ def pytest_configure(config):
           junit_family,
           config.getini("junit_log_passing_tests"),
       )
-      config.pluginmanager.register(config._xml)
+      config.pluginmanager.register(config._store[xml_key])
   # --------------------------------------------------------------------------------
 
 # --------------------------------------------------------------------------
@@ -178,10 +172,10 @@ def pytest_unconfigure(config):
     del config._html
     config.pluginmanager.unregister(html)
 
-  xml = getattr(config, "_xml", None)
+  xml = config._store.get(xml_key, None)
   if xml:
-    del config._xml
-    config.pluginmanager.unregister(xml)
+      del config._store[xml_key]
+      config.pluginmanager.unregister(xml)
 
   _mpi_log = getattr(config, "_mpi_log", None)
   if _mpi_log:
