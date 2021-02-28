e9068bb068ae72182c1c5d2000625b4f54e59ca6
Bruno Maugars <bruno.maugars@onera.fr>
2021-02-28 08:44:17 +0100

    Lambda function assert_mpi

--- pytest_mpi_check/__init__.py
+++ pytest_mpi_check/__init__.py
@@ -3,8 +3,11 @@ __version__ = "0.1"
 
 # export PYTHONUNBUFFERED=true
 
-def assert_mpi(comm, rank, cond ):
+def assert_mpi(comm, rank, cond, *args ):
   if(comm.rank == rank):
-    assert(cond == True)
+    if(isinstance(cond, bool)):
+      assert(cond == True)
+    else:
+      assert(cond(*args) == True)
   else:
     pass
--- pytest_mpi_check/_decorator.py
+++ pytest_mpi_check/_decorator.py
@@ -9,16 +9,6 @@ def sub_comm(request):
   """
   return request._pyfuncitem._sub_comm
 
-# def exit_if_null_comm(tested_fun):
-#   def exit_if_null_comm_impl(_,sub_comm,*args,**kwargs):
-#     if sub_comm == MPI.COMM_NULL :
-#       return
-#     print(tested_fun)
-#     tested_fun(sub_comm,*args,**kwargs)
-#   # preserve the tested_fun named arguments signature for use by other pytest decorator
-#   tested_fun_replica = decorate(tested_fun,exit_if_null_comm_impl)
-#   return tested_fun_replica
-
 def mark_mpi_test(n_proc_list):
   if type(n_proc_list)==int: # Replace by isinstance
     n_proc_list = [n_proc_list]
